d10ff835bae16fac8ef21bfd3297df026144d537
Elenaelle05 <118297004+Elenaelle05@users.noreply.github.com>
2023-12-15 16:08:56 +0000

    task 8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector(".login-form");
+
+const object = {}
+const handlerFormSumbit = (event) => {
+  event.preventDefault();
+  const emailInputValue = event.currentTarget.elements.email.value;
+  console.log("emailInput", emailInputValue);
+  const passwordInputValue = event.currentTarget.elements.password.value;
+  console.log("passwordInput", passwordInputValue);
+  if (emailInputValue === "" || passwordInputValue === "") {
+    alert("всі поля повинні бути заповнені.")
+    return;
+  }
+  object.email = emailInputValue;
+  object.password = passwordInputValue;
+  console.log("object", object);
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handlerFormSumbit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const buttonRef = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+const bodyRef = document.querySelector("body");
+
+const handleChange = (e) => {
+  // console.log(getRandomHexColor());
+  bodyRef.style.backgroundColor = getRandomHexColor(); 
+  spanRef.innerHTML = getRandomHexColor();
+};
+
+buttonRef.addEventListener("click", handleChange);
