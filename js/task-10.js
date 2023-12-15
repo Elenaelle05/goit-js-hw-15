684706451e54426a8a757360b31d00bb5c098809
Elenaelle05 <118297004+Elenaelle05@users.noreply.github.com>
2023-12-15 16:36:08 +0000

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const container = document.querySelector("#boxes");
+
+let amount = 0;
+createButton.addEventListener("click", () => createBoxes(amount));
+destroyButton.addEventListener("click", destroyBoxes);
+input.addEventListener("input", handleChange);
+
+function handleChange(e) { 
+  amount = e.target.value
+  console.log("amount", amount);
+};
+
+function destroyBoxes() { 
+  container.innerHTML = '';
+};
+
+function createBoxes(amount) {
+  console.log("amount", amount);
+  let box = '';
+  for (let index = 0; index < amount; index++) {
+    const width = index * 10;
+    box += <div style="width: ${width}px; height: ${width}px; background-color: ${getRandomHexColor()} "></div>
+  }
+
+  container.innerHTML = box;
+ };
