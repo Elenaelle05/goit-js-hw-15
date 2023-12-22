5fbb508443ab45f33da471e11b6e439a4576e628
Elenaelle05 <118297004+Elenaelle05@users.noreply.github.com>
2023-12-22 11:03:20 +0000

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const nameInput= document.querySelector("#font-size-control")
 const nameSpan = document.querySelector("#text")
-console.log(nameInput);
+
 const handleChange = (event) => {
   nameSpan.style.fontSize = event.target.value + "px"
 }
